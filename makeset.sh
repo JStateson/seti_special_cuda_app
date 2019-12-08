543cec52f9722bbdbb1a1c18713dc826b14af395
jstateson <josephy@stateson.net>
2019-12-08 10:47:12 -0600

    inital setup

---
+++ makeset.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# place this bash script in the client folder of Tbar's Xbranch
+# this script uses your defined folder locations to create  "setvars.sh"
+# setvars.sh first renames the Makefile to Makefile.bu then sets your locations into it
+# first run "setvars.sh" then run "make" to build the 10.2 libraries
+# edit the following two lines to locate your own folders
+BOINCDIR="/home/jstateson/Projects/BoincMasterSlave"
+SETI_DIR="/home/jstateson/Projects/Xbranch"
+echo "\
+#!/bin/bash
+FILE=Makefile.bu
+if [ ! -f "$FILE" ]; then
+  cp Makefile Makefile.bu
+fi
+sed 's=/home/tbar/boinc/="$BOINCDIR"/=g' ../Makefile.bu | \
+	sed 's=/home/tbar/boinc="$BOINCDIR"=g' | \
+	sed 's=/home/tbar/sah_v7_opt/Xbranch="$SETI_DIR"=g' | \
+	sed 's/cuda-10.1/cuda-10.2/g' > Makefile
+" > setvars.sh
+chmod +x setvars.sh
+
