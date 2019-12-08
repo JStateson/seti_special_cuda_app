7509f9d0a5397ce49af1ad3b48944632339b17d9
Joseph Stateson <josephy@stateson.net>
2019-12-08 13:12:35 -0600

    Update makeset.sh

--- makeset.sh
+++ makeset.sh
@@ -3,7 +3,7 @@
 # place this bash script in the client folder of Tbar's Xbranch
 # this script uses your defined folder locations to create  "setvars.sh"
 # setvars.sh first renames the Makefile to Makefile.bu then sets your locations into it
-# first run "setvars.sh" then run "make" to build the 10.2 libraries
+# first run "setvars.sh" then run "make" to build the 10.2 seti app
 # edit the following two lines to locate your own folders
 BOINCDIR="/home/jstateson/Projects/BoincMasterSlave"
 SETI_DIR="/home/jstateson/Projects/Xbranch"
